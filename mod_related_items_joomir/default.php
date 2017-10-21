c37a779af2e89c195cc533e03bfa870b170ca398
Uziel Almeida Oliveira <uzielweb@gmail.com>
2017-10-21 01:46:35 -0200

    Add files via upload

---
+++ mod_related_items_joomir/default.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @package      Joomla.Site
+ * @subpackage   mod_related_items
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license      GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+$app = JFactory::getApplication();
+
+$widthchoosen = $params->get('widthchoosen', '360');
+$heightchoosen = $params->get('heightchoosen', '240');
+$cropchoosen = $params->get('cropchoosen', '4:3');
+$tpath = JURI::base(true).'/templates/'.$app->getTemplate().'/';
+
+$modulepath = $tpath.'/html/mod_'.$module->name.'/';
+$thumbsnippet = $modulepath.'/assets/smart/image.php?width='.$widthchoosen.'&height='.$heightchoosen.'&cropratio='.$cropchoosen.'&image='.JURI::root();
+
+$document = JFactory::getDocument();
+$document->addStyleSheet($modulepath.'owlcarousel/owl.carousel.min.css');
+$document->addStyleSheet($modulepath.'owlcarousel/owl.theme.default.min.css');
+$document->addStyleSheet($modulepath.'assets/style.css');
+//$document->addScript($modulepath.'owlcarousel/jquery.min.js');
+$document->addScript($modulepath.'owlcarousel/owl.carousel.min.js');
+$document->addScriptDeclaration('
+jQuery(document).ready(function($){
+  $(".owl-carousel").owlCarousel({
+	 loop:true,
+    margin:10,
+    nav:true,
+    buttons: true,
+    thumbnails: true,
+    autoplay: true
+  
+  });
+});
+'); 
+?>
+<!--<h3 class="paleo_title">Related Items</h3>-->
+
+<div class="owl-carousel">
+  
+<?php 
+$i = 0;
+foreach ($list as $item) : { if (++$i > 5){break ;}   ?>
+<?PHP
+      // Grab intro text and shorten to length specified below in numwords
+  $numwords = 35;
+  preg_match("/(\S+\s*){0,$numwords}/", $item->introtext, $regs);
+  $shortdesc = trim($regs[0]);
+?>
+
+
+
+<?php 
+      // Use json decode for image object
+$image = json_decode($item->images);
+ ?>
+<div id="paleo-related-wrapper">
+
+
+<div id="paleo-related-item-with-image" class="col-md-3 col-sm-6 col-xs-12">
+
+<span class="related-image">
+<a href="<?php echo $item->route; ?>">
+<img src="<?php echo $thumbsnippet.$image->{'image_intro'}; ?>" class="related-resize" /></a>
+</span>   
+
+<div class="related-link">
+<a href="<?php echo $item->route; ?>"><?php echo $item->title; ?></a>
+</div>
+
+<!--
+<span class="related-text">
+<?php echo $shortdesc ; ?>
+</span>      
+-->
+</div>
+
+</div>
+<?php 
+}      
+ endforeach; ?>
+</div>
